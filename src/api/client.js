4f593bb514ae728b532c31588fb7fa644327209b
Kevin <klzheng@buffalo.edu>
2022-10-28 13:27:10 -0400

    changed browser router to hash router moved project demo gif

--- src/api/client.js
+++ src/api/client.js
@@ -3,7 +3,9 @@ import axios from "axios";
 
 // uses axios to send http requests to endpoints
 const client = axios.create({
-  baseURL: "http://localhost:8000/api",
+  // baseURL: "http://localhost:8000/api",
+  // baseURL: "https://seal-app-odxdb.ondigitalocean.app/api"
+  baseURL: process.env.REACT_APP_BASE_URL
 });
 
 export default client;
